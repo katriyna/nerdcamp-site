572be94e06f0c4b169ad25aec8af38bd537f9d1c
katriyna <katriyna@gmail.com>
2016-12-03 02:30:48 +0300

    shuffle photos

--- script/nc-2016.js
+++ script/nc-2016.js
@@ -2,35 +2,13 @@ require('./../style/nc-2016.scss');
 
 var $ = require('jquery');
 
-var React = require('react');
-var render = require('react-dom').render;
-
 $(function() {
   $('body').append(require('./../template/nc-2016.html'));
   initIcons();
 
   var gallery = require('./photo-gallery.jsx');
   const PHOTO_SET = require('./photo-set-constant');
-  gallery(PHOTO_SET);
-
-  // console.log('Gallery', Gallery);
-  // var gallery = React.createElement(Gallery.name, {});
-  // debugger;
-  // gallery.setState({photos: PHOTO_SET});
-  //
-  // render(
-  //   gallery,
-  //   document.getElementById('past-events-gallery-placeholder')
-  // );
-
-  // export default class Sample extends React.Component {
-  //   render() {
-  //     return (
-  //       <Gallery photos={PHOTO_SET} />
-  //   );
-  //   }
-  // }
-
+  gallery(shuffle(PHOTO_SET));
 });
 
 function initIcons() {
@@ -38,4 +16,14 @@ function initIcons() {
     var glyph = require('./../assets/' + iconName + '.svg');
     $('.nerd-camp__icon_' + iconName).append('<svg style="width: 18px; height: 18px;"><use xlink:href="'+ glyph + '"/></svg>');
   });
+}
+
+function shuffle(array) {
+  const resultArray = new Array(array.length);
+  for (var i = array.length; i > 0; --i) {
+    const j = Math.floor(Math.random() * i);
+    resultArray[i - 1] = array[j];
+    resultArray[j] = array[i - 1];
+  }
+  return resultArray;
 }

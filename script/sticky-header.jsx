feb972a0b24f05589cf07819232b0fdd90606694
katriyna <katriyna@gmail.com>
2019-06-13 01:59:39 +0300

    navigation menu redesign

--- script/sticky-header.jsx
+++ script/sticky-header.jsx
@@ -12,28 +12,22 @@ var NCStickyHeader = React.createClass({
 
   getInitialState: function () {
     return {
-      isFixed: false,
-      height: null
+      hasBorder: false,
+      isSmallScreen: window.innerWidth < 600
     };
   },
 
   getScrollHandler: function(node) {
     var DEBOUNCE_INTERVAL = 10;
-    var self = this;
 
-    var savedToolbarTop;
     if (!this.toolbarScrollListener) {
-      this.toolbarScrollListener = debounce(function () {
+      this.toolbarScrollListener = debounce(() => {
         var scrolledTop = (window.document.documentElement && window.document.documentElement.scrollTop) || window.document.body.scrollTop;
         var elementTop = node.getBoundingClientRect().top + scrolledTop;
-        var toolbarTop = savedToolbarTop || elementTop;
-        if (scrolledTop > toolbarTop && !savedToolbarTop) {
-          savedToolbarTop = toolbarTop;
-          //save height to style to prevent collapsing after fixing controls
-          self.setState({isFixed: true, height: node.offsetHeight + 'px'});
-        } else if (scrolledTop <= toolbarTop && savedToolbarTop >= 0) {
-          savedToolbarTop = null;
-          self.setState({isFixed: false, height: null});
+        if (elementTop && !this.state.hasBorder) {
+          this.setState({hasBorder: true, isSmallScreen: window.innerWidth < 600});
+        } else if (!elementTop && this.state.hasBorder) {
+          this.setState({hasBorder: false, isSmallScreen: window.innerWidth < 600});
         }
       }, DEBOUNCE_INTERVAL);
     }
@@ -52,42 +46,38 @@ var NCStickyHeader = React.createClass({
   },
 
   render: function () {
-    var className = '';
-    if (window.innerWidth > 600) {
-      className = 'nerd-camp__sticky-panel';
-      if (this.state.isFixed) {
-        className += ' nerd-camp__sticky-panel_fixed';
-      }
-    } else {
-      className = 'nerd-camp__no-sticky-panel';
+    const {hasBorder, isSmallScreen} = this.state;
+    const classNames = ['nerd-camp__sticky-panel'];
+    if (hasBorder) {
+      classNames.push('nerd-camp__sticky-panel_with-border');
     }
-    var style = this.state.height ? {height: this.state.height} : {};
+    if (isSmallScreen) {
+      classNames.push('nerd-camp__sticky-panel_small-screen');
+    }
 
     return (
-      <div style={style}>
-        <div className={className} ref="contentPlaceholder">
-          <span className="nerd-camp__link nerd-camp__link_about-nerd-camp">
-            О формате
-          </span>
-          <span className="nerd-camp__link nerd-camp__link_speakers">
-            Доклады
-          </span>
-          <span className="nerd-camp__link nerd-camp__link_past-nerd-camps">
-            Как это было
-          </span>
+      <div className={classNames.join(' ')} ref="contentPlaceholder">
+        <span className="nerd-camp__link nerd-camp__link_about-nerd-camp">
+          О формате
+        </span>
+        <span className="nerd-camp__link nerd-camp__link_speakers">
+          Доклады
+        </span>
+        <span className="nerd-camp__link nerd-camp__link_past-nerd-camps">
+          Как это было
+        </span>
 
-          <span className="nerd-camp__pull-right">
-              <span>
-                  <a href="mailto:nerdcamp.info@gmail.com" target="_blank" className="nerd-camp__link nerd-camp__icon_mail"></a>
-              </span>
-              <span>
-                  <a href="https://vk.com/nc2017" target="_blank" className="nerd-camp__link nerd-camp__icon_vk"></a>
-              </span>
-              <span>
-                  <a href="https://telegram.me/nerdcamp2017" target="_blank" className="nerd-camp__link nerd-camp__icon_telegram"></a>
-              </span>
+        <span className="nerd-camp__pull-right">
+          <span>
+              <a href="mailto:nerdcamp.info@gmail.com" target="_blank" className="nerd-camp__link nerd-camp__icon_mail"></a>
           </span>
-        </div>
+          <span>
+              <a href="https://vk.com/nc2017" target="_blank" className="nerd-camp__link nerd-camp__icon_vk"></a>
+          </span>
+          <span>
+              <a href="https://telegram.me/nerdcamp2017" target="_blank" className="nerd-camp__link nerd-camp__icon_telegram"></a>
+          </span>
+        </span>
       </div>
     );
   }
